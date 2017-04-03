25a02f53ba40679b154be8f37f37ced45ef7bcae
emil2001 <emilek2001@yandex.ru>
2017-04-03 13:45:57 +0400

    Update сапёр.py

--- сапёр.py
+++ сапёр.py
@@ -1,5 +1,6 @@
 from tkinter import*
 import random
+import time
 root = Tk()
 canvas = Canvas(root,width = 500, height = 500)
 canvas.pack(fill= BOTH)
@@ -12,69 +13,115 @@ jfield = 20
 n = 20
 m = 20
 list = [[000] * (m+1) for i in range(n+1)]
-k = 75
+k = 50
 x = random.randint(0, m - 1)
 y = random.randint(0, n - 1)
 list[y][x] = 100
 queue = [(x, y)]
 
 
-def dfs():
-    global queue,ifield,jfield,listA
 
+def game_over():
+    global list
+    for j in range(len(list)):
+        for i in range(len(list[j])):
+            if_mine = list[j][i]
+            if if_mine // 100 == 1:
+                draw_mine(i, j)
+
+
+def dfs(i,j):
+    global queue, ifield, jfield, list
+    if list[j][i]//100 == 1 and (list[j][i]//10)%10 !=2:
+        game_over()
+        return 0
+    elif (list[j][i] // 10) % 10 == 0 and list[j][i] % 10 == 0 and list[j][i] // 100 == 0:
+        open(i,j)
+    elif list[j][i] % 10 != 0 and (list[j][i] // 10) % 10 == 0 and list[j][i]//100 == 0:
+        canvas.create_rectangle(50 + i * 20, 50 + j * 20, 50 + (i + 1) * 20, 50 + (j + 1) * 20, fill='red')
+        canvas.create_text(60 + i * 20, 60 + j * 20, text=list[j][i] % 10)
+        time.sleep(0.05)
+        canvas.update()
+        list[j][i] = list[j][i] // 100 + 10 + list[j][i] % 10
+
+
+def open(i,j):
+    global queue, ifield, jfield, list
+
+    canvas.create_rectangle(50 + i * 20, 50 + j * 20, 50 + (i + 1) * 20, 50 + (j + 1) * 20, fill='red')
+    time.sleep(0.05)
+    canvas.update()
+    list[j][i] = list[j][i] // 100 + 10 + list[j][i] % 10
+    if i >= 1:
+        if j >= 1:
+            dfs(i - 1, j - 1)
+        dfs(i - 1, j)
+        if j < len(list) - 2:
+            dfs(i - 1, j + 1)
+    if i < len(list[j]) - 2:
+        if j >= 1:
+            dfs(i + 1, j - 1)
+        dfs(i + 1, j)
+        if j < len(list) - 2:
+            dfs(i + 1, j + 1)
+    if j >= 1:
+        dfs(i, j - 1)
+    if j < len(list) - 2:
+        dfs(i, j + 1)
+
+
+
+def counter():
+    global queue,ifield,jfield,list
+    list = [[000] * (m + 1) for i in range(n + 1)]
     for i in range(k):
         x = random.randint(1, m - 2)
         y = random.randint(1, n - 2)
         list[y][x] = 100
         queue += [(x, y)]
-    #print(queue)
-    # for i in range(n):
-    #    for j in range(m):
-    #        print(list[i][j], end = ' ')
-    #    print()
-    for i in range(1,ifield):
-        for j in range(1,jfield):
+    for i in range(0,ifield):
+        for j in range(0,jfield):
             x1 = i
             y1 = j
             mine_count = (list[y1 - 1][x1 - 1] + list[y1][x1 - 1] + list[y1 + 1][x1 - 1] + list[y1 + 1][x1] + list[y1 - 1][x1] +
                           list[y1 - 1][x1 + 1] + list[y1][x1 + 1] + list[y1 + 1][x1 + 1]) // 100
-            #print(mine_count)
             if_mine = list[y1][x1]
-            if if_mine == 100:
-                draw_mine(x1,y1)
-            #print(mine_count)
             list[y1][x1] = if_mine + mine_count
 
-    #print(list)
-
-def field_open(x,y):
+def bfs(x,y):
     global list
     queue = [(x,y)]
+    if list[y][x]//100 == 1:
+        game_over()
+        return 0
     while len(queue) >0:
         i,j = queue.pop(0)
         if list[j][i]%10 != 0 and (list[j][i] // 10) % 10 == 0 :
             canvas.create_rectangle(50 + i * 20, 50 + j * 20, 50 + (i + 1) * 20, 50 + (j + 1) * 20, fill='red')
             canvas.create_text(60+i*20,60+j*20,text = list[j][i]%10)
-            #print('game over')
+            time.sleep(0.05)
+            canvas.update()
             list[j][i] = list[j][i] // 100 + 10 + list[j][i] % 10
         if (list[j][i] // 10) % 10 == 0 and list[j][i]%10 ==0 and list[j][i]//100 == 0:
             canvas.create_rectangle(50+i*20,50+j*20,50+(i+1)*20,50+(j+1)*20, fill = 'red')
+            time.sleep(0.05)
+            canvas.update()
             list[j][i] = list[j][i]//100 + 10 + list[j][i]%10
             if i>=1:
                 if j>=1:
                     queue += [(i-1,j-1)]
                 queue += [(i-1, j)]
-                if j<len(list)-1:
+                if j<len(list)-2:
                     queue += [(i-1, j+1)]
             if i<len(list[j])-2:
                 if j>=1:
                     queue += [(i+1, j-1)]
                 queue += [(i+1, j)]
-                if j<len(list)-1:
+                if j<len(list)-2:
                     queue += [(i+1, j+1)]
             if j >= 1:
                 queue += [(i, j - 1)]
-            if j < len(list)-1:
+            if j < len(list)-2:
                 queue += [(i, j + 1)]
 
 
@@ -82,9 +129,9 @@ def field_open(x,y):
 
 
 def draw_flag(x,y):
-    canvas.create_polygon(58+x*20,50+y*20,67+x*20,55+y*20,58+x*20,60+y*20,fill = 'red')
-    canvas.create_line(58+x*20,50+y*20,58+x*20,65+y*20)
-    canvas.create_arc(52+x*20,65+y*20,68+x*20,75+y*20,start = 0,extent = 180,fill = 'black',style = CHORD)
+    canvas.create_polygon(58+x*20,50+y*20,67+x*20,55+y*20,58+x*20,60+y*20,fill = 'red',tag = [x,y])
+    canvas.create_line(58+x*20,50+y*20,58+x*20,65+y*20,tag = [x,y])
+    canvas.create_arc(52+x*20,65+y*20,68+x*20,75+y*20,start = 0,extent = 180,fill = 'black',style = CHORD, tag = [x,y])
 def draw_mine(x,y):
     canvas.create_oval(50+x*20,50+y*20,70+x*20,70+y*20,fill = 'black',tag = 'sqwer')
 def create_field():
@@ -101,28 +148,31 @@ def but1(event):
     global xreset,yreset,square
     x = (event.x - 50) // 20
     y = (event.y - 50) // 20
-    #draw_mine(x,y)
-    field_open(x,y)
+    dfs(x,y)
 
 
     canvas.update()
 def clear(event):
     canvas.create_rectangle(10, 10, 10+xreset, 10+yreset, fill='#d1cdcd',tag = 'New',outline = '#d1cdcd')
-    #canvas.create()
     canvas.delete('field')
     canvas.delete('New')
     create_field()
+    counter()
 def but3(event):
     global list
     x = (event.x - 50) // 20
     y = (event.y - 50) // 20
-    #canvas.create_rectangle(50+x*20,50+y*20,70+x*20,70+y*20,fill = 'blue',tag = 'sqwer')
-    draw_flag(x,y)
-    list[y][x] = list[y][x] + 20
+    if list[y][x]//10%10 == 2:
+        canvas.delete([x,y])
+        canvas.update()
+        list[y][x] = list[y][x] - 20
+    elif list[y][x]//10%10 == 0:
+        draw_flag(x, y)
+        list[y][x] = list[y][x] + 20
     print(list[y][x])
     canvas.update()
 create_field()
-dfs()
+counter()
 canvas.tag_bind('field','<Button-1>',but1)
 canvas.tag_bind('New','<Button-1>',clear)
 canvas.tag_bind('field','<Button-3>',but3)
