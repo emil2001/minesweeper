ffe59e565950dbc9a6bafd6e2f3694fcedf4e522
- <emilek2001@yandex.ru>
2017-04-24 12:39:02 +0300

    sad

--- pool.py
+++ pool.py
@@ -35,8 +35,8 @@ class stick:
         canv.create_line(self.xb+math.cos(self.alp)*self.v,self.yb-math.sin(self.alp)*self.v,self.xb+math.cos(self.alp)*self.v+math.cos(self.alp)*self.l,self.yb-math.sin(self.alp)*self.v-math.sin(self.alp)*self.l,width=3,fill = 'brown',tag = 'stick')
     def rotate(self,angle):
         self.alp += math.radians(angle)
-    def select(self,N):
-        self.N = N
+ #   def select(self,N):
+ #       self.N = N
     def redraw(self):
         canv.delete('stick')
         self.xb = a.x
@@ -57,7 +57,9 @@ class ball:
         self.vx = 0
         self.vy = 0
         self.v = 0
+        self.alp = 0
     def move(self):
+
         self.x += self.vx
         self.y += self.vy
         if (-2 < self.v and self.v < 2):
@@ -65,42 +67,60 @@ class ball:
         else:
             self.v -=1
         if self.vx <0:
-            self.vx =- self.v*math.cos(stick.alp)
+            self.vx = -self.v*math.cos(self.alp)
         elif self.vx >0:
-            self.vx =- self.v * math.cos(stick.alp)
+            self.vx = -self.v * math.cos(self.alp)
         if self.vy<0:
-            self.vy = self.v * math.sin(stick.alp)
+            self.vy = self.v * math.sin(self.alp)
         elif self.vy>0:
-            self.vy = self.v * math.sin(stick.alp)
+            self.vy = self.v * math.sin(self.alp)
+    def collide(self):
+        s = math.sqrt((a.x-b.x)**2 +(a.y-b.y)**2)
+        if b.y - a.y >0:
+            alp1 = math.acos((b.x-a.x)/s)
+        else:
+            alp1 = -math.acos((b.x-a.x)/s)
+        #if
+        #if s == a.r+b.r:
+           # a.v = a.v +
+    def border(self):
+        if self.y - self.r <= 0:
+            self.vy = -self.vy
+            self.alp =( math.radians(90) - (self.alp - math.radians(180)))*2+self.alp
+            #self.v = math.sqrt(self.vx**2+self.vy**2)
+
 
 
     def change_sp(self, x,y):
         self.vx+=x
         self.vy+=y
     def draw(self):
-        canv.delete(N)
-        canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = self.color, tag = N)
+        canv.delete(self.N)
+        #canv.update()
+        canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = self.color, tag = self.N)
 
 alp = 50
 v = 0
-a = ball(200, 200, 10, "black",1)
+a = ball(200, 200, 10, "black",'a')
+b = ball(300, 200, 10, "green",'b')
 table = table('green', 'brown',400,200,300,100)
-stick = stick(1,alp,v,100,'white',a.x,a.y)
+stick = stick(1,alp,v,200,'white',a.x,a.y)
 
-def move_top(event):
-    print(1)
-    global a,stick
-    a.change_sp(0,-10)
-    stick.redraw()
+#def move_top(event):
+  #  print(1)
+   # global a,stick
+ #   a.change_sp(0,-10)
+  #  stick.redraw()
 def change_angle(event):
     global stick
     stick.rotate(5)
     stick.draw()
+    a.alp = stick.alp
 def change_angle_reversed(event):
     global stick
     stick.rotate(-5)
     stick.draw()
-
+    a.alp = stick.alp
 
 def STRIKE( event):
     a.vx = -math.cos(stick.alp) * stick.v/5
@@ -113,17 +133,21 @@ def STRIKE( event):
 
 root.bind("<Up>", change_angle_reversed)
 root.bind("<Down>", change_angle)
-root.bind("<Left>",move_top)
+#root.bind("<Left>",move_top)
 root.bind("<BackSpace>", stick.charge)
 root.bind("<space>",STRIKE)
 
 
-
+a.alp = stick.alp
 
 while True:
     #table.draw()
+    a.border()
     a.move()
+    b.move()
     a.draw()
+    b.draw()
+
     if a.vx == 0 and a.vy == 0:
         stick.redraw()
     canv.update()
